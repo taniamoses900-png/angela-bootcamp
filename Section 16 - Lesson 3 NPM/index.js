b92c8dd90516dbcf81df06c17be2bf8dc05c2c33
Tania Moses <taniamoses@Tanias-MacBook-Air.local>
2026-06-30 16:03:18 +0200

    feat(node): generate random superhero name using package

---
+++ Section 16 - Lesson 3 NPM/index.js
@@ -0,0 +1,13 @@
+// var generateName = require("sillyName");
+
+// import generateName from "sillyname";
+// var sillyName = generateName();
+
+// console.log(`My name is ${sillyName}.`);
+
+
+import { randomSuperhero } from 'superheroes';
+
+var superheroName = randomSuperhero();
+
+console.log("I am " + superheroName + "!");
