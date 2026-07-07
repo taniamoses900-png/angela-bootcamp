b564172bd23bc878dd7c38de32eb1691eb325b8f
Tania Moses <taniamoses@Tanias-MacBook-Air.local>
2026-07-07 19:05:28 +0200

    feat(db): implement fuzzy text matching for country searches

--- Section 25 - Lesson 3 Travel Tracker/index.js
+++ Section 25 - Lesson 3 Travel Tracker/index.js
@@ -19,39 +19,30 @@ app.use(express.static("public"));
 
 async function checkVisisted() {
   const result = await db.query("SELECT country_code FROM visited_countries");
-
   let countries = [];
   result.rows.forEach((country) => {
     countries.push(country.country_code);
   });
   return countries;
 }
-
+// GET home page
 app.get("/", async (req, res) => {
   const countries = await checkVisisted();
   res.render("index.ejs", { countries: countries, total: countries.length });
 });
 
+//INSERT new country
 app.post("/add", async (req, res) => {
   const input = req.body["country"];
 
   try {
     const result = await db.query(
-      "SELECT country_code FROM countries WHERE country_name ILIKE $1",
-      [input.trim()]
+      "SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%';",
+      [input.toLowerCase()]
     );
 
-    if (result.rows.length === 0) {
-      const countries = await checkVisisted();
-      return res.render("index.ejs", {
-        countries: countries,
-        total: countries.length,
-        error: "Country name does not exist, try again.",
-      });
-    }
-
-    const countryCode = result.rows[0].country_code;
-
+    const data = result.rows[0];
+    const countryCode = data.country_code;
     try {
       await db.query(
         "INSERT INTO visited_countries (country_code) VALUES ($1)",
@@ -69,7 +60,12 @@ app.post("/add", async (req, res) => {
     }
   } catch (err) {
     console.log(err);
-    res.status(500).send("Internal Server Error");
+    const countries = await checkVisisted();
+    res.render("index.ejs", {
+      countries: countries,
+      total: countries.length,
+      error: "Country name does not exist, try again.",
+    });
   }
 });
 
