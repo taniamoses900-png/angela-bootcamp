744d940d76f68544c5fc410e5a9c0e2b160582f5
Tania Moses <taniamoses@Tanias-MacBook-Air.local>
2026-07-07 18:53:52 +0200

    chore(db): implement multi-user tracking logic for travel tracker part 2

--- Section 25 - Lesson 3 Travel Tracker/index.js
+++ Section 25 - Lesson 3 Travel Tracker/index.js
@@ -17,16 +17,50 @@ db.connect();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-
-app.get("/", async (req, res) => {
+async function checkVisited() {
   const result = await db.query("SELECT country_code FROM visited_countries");
   let countries = [];
   result.rows.forEach((country) => {
     countries.push(country.country_code);
   });
-  console.log(result.rows);
+  return countries;
+}
+
+app.get("/", async (req, res) => {
+  const countries = await checkVisited();
   res.render("index.ejs", { countries: countries, total: countries.length });
-  db.end();
+});
+
+app.post("/add", async (req, res) => {
+  const input = req.body["country"];
+
+  try {
+    const result = await db.query(
+      "SELECT country_code FROM countries WHERE country_name ILIKE $1",
+      [input]
+    );
+
+    if (result.rows.length !== 0) {
+      const data = result.rows[0];
+      const countryCode = data.country_code;
+
+      try {
+        await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [
+          countryCode,
+        ]);
+        res.redirect("/");
+      } catch (err) {
+        console.log("Error inserting into database:", err);
+        res.redirect("/");
+      }
+    } else {
+      console.log("Country not found.");
+      res.redirect("/");
+    }
+  } catch (err) {
+    console.log("Error querying database:", err);
+    res.redirect("/");
+  }
 });
 
 app.listen(port, () => {
