ffeb721ceaf023584992d86ef1f4bc09f8cb4760
Tania Moses <taniamoses@Tanias-MacBook-Air.local>
2026-07-12 19:20:29 +0200

    feat(react): complete es6 calculator import and export practice

--- react-bootcamp/my-react-app/src/App.jsx
+++ react-bootcamp/my-react-app/src/App.jsx
@@ -1,11 +1,12 @@
-import pi, { doublePi, triplePi } from "./math.js";
+import { add, multiply, subtract, divide } from "./calculator.js";
 
 function App() {
   return (
     <ul>
-      <li>{pi}</li>
-      <li>{doublePi()}</li>
-      <li>{triplePi()}</li>
+      <li>{add(1, 2)}</li>
+      <li>{multiply(2, 3)}</li>
+      <li>{subtract(7, 2)}</li>
+      <li>{divide(5, 2)}</li>
     </ul>
   );
 }
