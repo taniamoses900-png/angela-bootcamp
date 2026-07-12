06b37efd70cbff1988bc9ff5664f0faf239ed2d3
Tania Moses <taniamoses@Tanias-MacBook-Air.local>
2026-07-12 19:59:45 +0200

    feat(react): display custom movie star contact cards using components and props

--- react-bootcamp/my-react-app/src/App.jsx
+++ react-bootcamp/my-react-app/src/App.jsx
@@ -1,14 +1,25 @@
-import "./App.css";
-import Header from "./components/Header.jsx";
-import Footer from "./components/Footer.jsx";
-import Note from "./components/Note.jsx";
+import Card from "./components/Card.jsx";
+import adamImg from "./assets/adam.jpg";
+import jimImg from "./assets/jim.jpg";
 
 function App() {
   return (
     <div>
-      <Header />
-      <Note />
-      <Footer />
+      <h1>My Contacts</h1>
+      
+      <Card 
+        name="Adam Sandler" 
+        img={adamImg} 
+        tel="+1 555 8831" 
+        email="adam@happygilmore.com" 
+      />
+
+      <Card 
+        name="Jim Carrey" 
+        img={jimImg} 
+        tel="+1 555 4429" 
+        email="jim@liarliar.com" 
+      />
     </div>
   );
 }
---
+++ react-bootcamp/my-react-app/src/components/Card.jsx
@@ -0,0 +1,12 @@
+function Card(props) {
+  return (
+    <div className="card">
+      <h2>{props.name}</h2>
+      <img src={props.img} alt="avatar_img" />
+      <p>{props.tel}</p>
+      <p>{props.email}</p>
+    </div>
+  );
+}
+
+export default Card;
