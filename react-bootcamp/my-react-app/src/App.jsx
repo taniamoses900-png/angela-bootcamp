f94d77b5c0778212c70bf192384a7e6202d53e55
Tania Moses <taniamoses@Tanias-MacBook-Air.local>
2026-07-12 18:38:19 +0200

    feat(react): add inline styling to hello world heading

--- react-bootcamp/my-react-app/src/App.jsx
+++ react-bootcamp/my-react-app/src/App.jsx
@@ -1,30 +1,14 @@
-import "./App.css";
-import baconImg from "./assets/bacon.jpg";
-import chickenImg from "./assets/chicken.jpg";
-import pastaImg from "./assets/pasta.jpg";
+const customStyle = {
+  color: "red",
+  fontSize: "20px",
+  border: "1px solid black"
+};
+
+customStyle.color = "blue";
 
 function App() {
   return (
-    <div className="main-wrapper">
-      <h1 className="heading">My Favourite Foods</h1>
-
-      <div className="food-container">
-        <div className="food-item">
-          <p>Bacon</p>
-          <img className="food-img" src={baconImg} alt="bacon" />
-        </div>
-
-        <div className="food-item">
-          <p>Pasta</p>
-          <img className="food-img" src={pastaImg} alt="pasta" />
-        </div>
-
-        <div className="food-item">
-          <p>Chicken</p>
-          <img className="food-img" src={chickenImg} alt="chicken" />
-        </div>
-      </div>
-    </div>
+    <h1 style={customStyle}>Hello World!</h1>
   );
 }
 
