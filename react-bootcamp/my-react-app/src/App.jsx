edb66316d7aa236723d7947f0dd0cdfd66c06db8
Tania Moses <taniamoses@Tanias-MacBook-Air.local>
2026-07-13 21:16:34 +0200

    feat(auth): implement conditional rendering with ternary and and operators

--- react-bootcamp/my-react-app/src/App.jsx
+++ react-bootcamp/my-react-app/src/App.jsx
@@ -1,24 +1,14 @@
 import React from "react";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Note from "./components/Note";
-import notes from "./notes";
+import Login from "./components/Login";
+
+var isLoggedIn = false;
 
 function App() {
   return (
-    <div>
-      <Header />
-      {notes.map(noteItem => (
-        <Note
-          key={noteItem.key}
-          title={noteItem.title}
-          content={noteItem.content}
-        />
-      ))}
-      <Footer />
+    <div className="container">
+      {isLoggedIn ? <h1>Hello</h1> : <Login />}
     </div>
   );
 }
 
 export default App;
-
