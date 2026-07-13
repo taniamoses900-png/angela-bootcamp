a205e48bd7a657490ba7302024c77f18212bfa30
Tania Moses <taniamoses@Tanias-MacBook-Air.local>
2026-07-13 21:08:35 +0200

    feat(keeper-pt2): render notes dynamically using array map

--- react-bootcamp/my-react-app/src/App.jsx
+++ react-bootcamp/my-react-app/src/App.jsx
@@ -1,47 +1,24 @@
 import React from "react";
-import emojipedia from "./emojipedia";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Note from "./components/Note";
+import notes from "./notes";
 
 function App() {
-  const numbers = Array.of(3, 56, 2, 48, 5);
-
-  const doubledNumbers = numbers.map(x => x * 2);
-  console.log(doubledNumbers);
-
-  const bigNumbers = numbers.filter(x => x > 10);
-  console.log(bigNumbers);
-
-  const sumOfNumbers = numbers.reduce((acc, curr) => acc + curr);
-  console.log(sumOfNumbers);
-
-  const firstBigNumber = numbers.find(x => x > 10);
-  console.log(firstBigNumber);
-
-  const firstBigNumberIndex = numbers.findIndex(x => x > 10);
-  console.log(firstBigNumberIndex);
-
-  const truncatedMeanings = emojipedia.map(emojiEntry => emojiEntry.meaning.substring(0, 100));
-  console.log(truncatedMeanings);
-
   return (
     <div>
-      <h1>
-        <span>emojipedia</span>
-      </h1>
-      <dl className="dictionary">
-        {emojipedia.map(emojiTerm => (
-          <div className="term" key={emojiTerm.id}>
-            <dt>
-              <span className="emoji" role="img" aria-label={emojiTerm.name}>
-                {emojiTerm.emoji}
-              </span>
-              <span>{emojiTerm.name}</span>
-            </dt>
-            <dd>{emojiTerm.meaning}</dd>
-          </div>
-        ))}
-      </dl>
+      <Header />
+      {notes.map(noteItem => (
+        <Note
+          key={noteItem.key}
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+      ))}
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
---
+++ react-bootcamp/my-react-app/src/notes.js
@@ -0,0 +1,24 @@
+const notes = [
+  {
+    key: 1,
+    title: "Delegation",
+    content: "Q. How many programmers does it take to change a light bulb? A. None – It’s a hardware problem"
+  },
+  {
+    key: 2,
+    title: "Loops",
+    content: "How to keep a programmer in the shower forever. Show him the shampoo instructions: Lather. Rinse. Repeat."
+  },
+  {
+    key: 3,
+    title: "Arrays",
+    content: "Why did the programmer quit his job? Because he didn't get arrays."
+  },
+  {
+    key: 4,
+    title: "Hardware vs. Software",
+    content: "What's the difference between hardware and software? You can hit hardware with a hammer; you can only curse at software."
+  }
+];
+
+export default notes;
