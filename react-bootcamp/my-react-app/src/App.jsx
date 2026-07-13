a507a9bdea8711593e78804203d1a350cda6a7d4
Tania Moses <taniamoses@Tanias-MacBook-Air.local>
2026-07-13 20:59:31 +0200

    refactor: convert array methods to es6 arrow functions

--- react-bootcamp/my-react-app/src/App.jsx
+++ react-bootcamp/my-react-app/src/App.jsx
@@ -4,34 +4,22 @@ import emojipedia from "./emojipedia";
 function App() {
   const numbers = Array.of(3, 56, 2, 48, 5);
 
-  const doubledNumbers = numbers.map(function (x) {
-    return x * 2;
-  });
+  const doubledNumbers = numbers.map(x => x * 2);
   console.log(doubledNumbers);
 
-  const bigNumbers = numbers.filter(function (x) {
-    return x > 10;
-  });
+  const bigNumbers = numbers.filter(x => x > 10);
   console.log(bigNumbers);
 
-  const sumOfNumbers = numbers.reduce(function (accumulator, currentNumber) {
-    return accumulator + currentNumber;
-  });
+  const sumOfNumbers = numbers.reduce((acc, curr) => acc + curr);
   console.log(sumOfNumbers);
 
-  const firstBigNumber = numbers.find(function (x) {
-    return x > 10;
-  });
+  const firstBigNumber = numbers.find(x => x > 10);
   console.log(firstBigNumber);
 
-  const firstBigNumberIndex = numbers.findIndex(function (x) {
-    return x > 10;
-  });
+  const firstBigNumberIndex = numbers.findIndex(x => x > 10);
   console.log(firstBigNumberIndex);
 
-  const truncatedMeanings = emojipedia.map(function (emojiEntry) {
-    return emojiEntry.meaning.substring(0, 100);
-  });
+  const truncatedMeanings = emojipedia.map(emojiEntry => emojiEntry.meaning.substring(0, 100));
   console.log(truncatedMeanings);
 
   return (
@@ -40,19 +28,17 @@ function App() {
         <span>emojipedia</span>
       </h1>
       <dl className="dictionary">
-        {emojipedia.map(function (emojiTerm) {
-          return (
-            <div className="term" key={emojiTerm.id}>
-              <dt>
-                <span className="emoji" role="img" aria-label={emojiTerm.name}>
-                  {emojiTerm.emoji}
-                </span>
-                <span>{emojiTerm.name}</span>
-              </dt>
-              <dd>{emojiTerm.meaning}</dd>
-            </div>
-          );
-        })}
+        {emojipedia.map(emojiTerm => (
+          <div className="term" key={emojiTerm.id}>
+            <dt>
+              <span className="emoji" role="img" aria-label={emojiTerm.name}>
+                {emojiTerm.emoji}
+              </span>
+              <span>{emojiTerm.name}</span>
+            </dt>
+            <dd>{emojiTerm.meaning}</dd>
+          </div>
+        ))}
       </dl>
     </div>
   );
