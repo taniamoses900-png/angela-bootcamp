2f417ff922c71809b164b817bda952a61a3b3cbb
Tania Moses <taniamoses@Tanias-MacBook-Air.local>
2026-07-14 09:09:32 +0200

    docs(components): implement side by side class and functional hooks comparison

--- react-bootcamp/my-react-app/src/App.jsx
+++ react-bootcamp/my-react-app/src/App.jsx
@@ -1,31 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
+import ClassComponent from "./ClassComponent";
 
 function App() {
-  const [name, setName] = useState("");
-  const [headingText, setHeadingText] = useState("");
-
-  function handleChange(event) {
-    setName(event.target.value);
-  }
-
-  function handleClick(event) {
-    setHeadingText(name);
-
-    event.preventDefault();
-  }
-
   return (
-    <div className="container">
-      <h1>Hello {headingText}</h1>
-      <form onSubmit={handleClick}>
-        <input
-          onChange={handleChange}
-          type="text"
-          placeholder="What's your name?"
-          value={name}
-        />
-        <button type="submit">Submit</button>
-      </form>
+    <div>
+      <ClassComponent />
     </div>
   );
 }
